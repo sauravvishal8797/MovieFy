01605d7acdfe5f35a74258a6aff7793a67307d99
Saurav Vishal <sauravvishal8797@gmail.com>
2017-08-21 18:01:40 +0530

    Added recyclerview layout for mainactivity

---
+++ app/src/main/java/android/example/com/popularmovies/Adapters/MoviesAdapter.java
@@ -0,0 +1,8 @@
+package android.example.com.popularmovies.Adapters;
+
+/**
+ * Created by saurav on 21/8/17.
+ */
+
+public class MoviesAdapter  {
+}
--- app/src/main/java/android/example/com/popularmovies/MainActivity.java
+++ app/src/main/java/android/example/com/popularmovies/MainActivity.java
@@ -1,13 +1,30 @@
 package android.example.com.popularmovies;
 
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(false);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        
+
+
+
     }
 }
